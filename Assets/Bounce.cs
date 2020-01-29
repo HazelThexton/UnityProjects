eba569fafbd77e97ff0898a401bd10e009d8b0da
Hazel Thexton <hazelthexton@videotron.ca>
2020-01-29 10:30:17 -0500

    Class 4

--- Assets/Bounce.cs
+++ Assets/Bounce.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Bounce : MonoBehaviour
 {
+    public GameObject the_other_object;
 
-public GameObject: the_other_object
     // Start is called before the first frame update
     void Start()
     {
@@ -18,15 +18,21 @@ public GameObject: the_other_object
         
     }
 
-	private void OnTriggerStay(Collider other)
-	{
-		other.GetComponent<Rigidbody>().AddForce(0,700.0f*Time.deltaTime,0);
-	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == the_other_object)
+        {
+            Debug.Log("Testing");
+            other.GetComponent<Rigidbody>().AddForce(0, 700.0f * Time.deltaTime, 0);
+        }
+    }
 
-private void OnTriggerEnter(Collider other)
-	{
-		if (other.gameObject == the_other_object){
-other.GetComponent<AudioSource>().Play();
-}
-	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == the_other_object)
+        {
+            other.GetComponent<AudioSource>().Play();
+        }
+        
+    }
 }
