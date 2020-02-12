6fcc8e7b3e1cf887fd0e42be6d174daba60ae188
Hazel Thexton <hazelthexton@videotron.ca>
2020-02-12 10:00:33 -0500

    Prototype2

---
+++ Assets/Scary.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scary : MonoBehaviour
+{
+    public GameObject Plane;
+    public AudioClip jaws;
+    public float speed;
+
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnControllerColliderHit(ControllerColliderHit hit)
+
+    {
+        if (hit.collider.gameObject.name == "Plane")
+        {
+            GetComponent<AudioSource>().PlayOneShot(jaws);
+        }
+        
+
+    }
+    }
